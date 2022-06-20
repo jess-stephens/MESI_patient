d7554cb2d3588916cc606adc09829020b15394ec
Jessica Stephens <64651019+jess-stephens@users.noreply.github.com>
2022-06-20 04:37:34 -0400

    loading data using rmd

---
+++ Scripts/00_setup.R
@@ -0,0 +1,49 @@
+## SETUP
+library(tidyverse)
+library(here)
+# 
+# 
+here()
+# folder_setup()
+
+library(gophr)
+library(glamr)
+library(readxl)
+library(janitor)
+
+library(dplyr)
+
+# install.packages("ggridges")
+# install.packages("glue")
+# install.packages("patchwork")
+# install.packages("RColorBrewer")
+# install.packages("vroom")
+# devtools::install_github("clauswilke/ggtext")
+# install.packages("tidytext")
+# install.packages("scales")
+
+
+#load librarys
+# 
+# library(gophr)
+# library(ICPIutilities)
+# library(tidyverse)
+# 
+# 
+# library(glamr)
+# library(glitr)
+# library(extrafont)
+# library(readxl)
+# library(here)
+# library(janitor)
+# 
+# library(tidytext)
+# library(scales) 
+# 
+# library(grid)
+# library(ggtext)
+# library(ggridges)
+# library(glue)
+# library(patchwork)
+# library(RColorBrewer)
+# library(vroom)
